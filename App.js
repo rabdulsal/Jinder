bf9fffcca52fbb03de9cba59d2cc0614751e4df2
Rashad Abdul-Salaam <rashad.abdul-salaam@uphs.upenn.edu>
2018-07-03 11:42:14 -0400

    First commit -- navigation & tutorial scaffolded

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import ReviewScreen from './screens/ReviewScreen';
+
+export default class App extends React.Component {
+  render() {
+    const MainNavigator = createBottomTabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: createBottomTabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: createStackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        })
+      }
+    });
+
+
+    return (
+      <MainNavigator /> // NOTE: Cannot wrap in <View> tag
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
